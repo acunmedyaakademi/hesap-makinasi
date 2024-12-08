0a178413e39dea03941848da489f3488ad6f2d7e
onur han hacimustafaoglu <onurhanhacimustafaoglu@gmail.com>
2024-12-08 22:06:34 +0300

    first commit

---
+++ hesap-makinasi.js
@@ -0,0 +1,24 @@
+// alert("Hesap Makinasına Hoşgeldiniz!");
+// let yapilacakIslem = prompt("Yapmak istediğiniz işlemi girin. (+), (-), (/), (*)");
+// let sayi1 = Number(prompt("1. sayiyi giriniz!"));
+// let sayi2 = Number(prompt("2. sayiyi giriniz!"));
+
+
+// if(yapilacakIslem === "*"){
+//     sonuc = sayi1 * sayi2;
+// }else if(yapilacakIslem === "+"){
+//     sonuc = sayi1 + sayi2;
+// }else if(yapilacakIslem === "-"){
+//     sonuc = sayi1 - sayi2;
+// }else if(yapilacakIslem === "/"){
+//     if(sayi2 === 0){
+//         alert("Bir sayıyı sıfıra bölemezsiniz!");
+//     }else{
+//         sonuc = sayi1 / sayi2;
+//     }
+// }else{
+//     alert("Geçersiz bir işlem girdiniz. Tekrar deneyiniz.")
+// }
+
+// alert(`Sonuç : ${sonuc}`);
+
